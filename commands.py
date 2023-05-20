0fd4299068caf7190e9b2054298a15df14a82c61
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-20 11:08:47 +0800

    Created the python files, text file, and the tentative pseudocodes base on what I want to see

---
+++ commands.py
@@ -0,0 +1,8 @@
+# Laguting, Maricon Jane G.
+# BSCpE 1-4
+# Task: Turn the Simple App Calculator program into OOP.
+
+# Display the brand of the calculator
+# Welcome the user
+# Print the inputted numbers and operations
+# store the inputs in calc_history text file
