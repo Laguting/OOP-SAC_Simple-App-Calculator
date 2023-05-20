281d036c9ac3f0de0b0005b10411f2adb848ab86
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-20 19:44:28 +0800

    Added the welcome feature.

--- calculator_interface.py
+++ calculator_interface.py
@@ -1,6 +1,9 @@
 from pyfiglet import Figlet
 from termcolor import colored
 class calc_interface():
+    def __init__(self, brand, welcome):
+        self.brand = brand
+        self.welcome = welcome
 # Brand of the Calculator
     def __init__(self, brand):
         self.brand = brand
@@ -12,4 +15,17 @@ class calc_interface():
         print(colored(s_a_c.renderText("                                                       Simple App Calculator"), "yellow"))
         return self.brand
 # Welcome the user
+    def show_welcome(self):
+        s_a_c = Figlet(font = "starwars", justify = "right")
+        print()
+        print(colored(s_a_c.renderText("            W e l c o m e              "), "green"))
+        print("\33[35m                                                                ┻┳|              \33[0m")
+        print("\33[35m                                                                ┳┻| _\33[0m")
+        print("\33[35m                                                                ┻┳|•.•) i'm watching.....\33[0m")
+        print("\33[35m                                                                ┳┻|  ⊂ﾉ)\33[0m")
+        print("\33[35m                                                                ┻┳|\33[0m")
+        print()
+        print("⚜ " * 89)
+        print()
+        return self.welcome
 # Loading Bar
