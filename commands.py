d5c8334949f89ae97762561935c3706617587e90
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-20 20:35:18 +0800

    Forgot to add the number input after asking the operation to the user.

--- commands.py
+++ commands.py
@@ -10,8 +10,9 @@ print(ci_1.show_welcome()) # Welcome the user
 print(ci_1.loading_bar()) # Loading Bar
 
 from user_interface import usr_intrfc
-ui_1 = usr_intrfc("")
+ui_1 = usr_intrfc("","")
 # Ask for the user's input
+print(ui_1.usr_operation())
 print(ui_1.usr_input())
 # Print the inputted numbers and operations
 # store the inputs in calc_history text file
--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,22 @@
 class usr_intrfc():
-    def __init__(self, input):
+    def __init__(self, operation, input):
+        self.operation = operation
         self.input = input
 # Ask for the user's initial inputs
-    def usr_input(self):
+    def usr_operation(self):   # Ask for operations
         try:
             ask_usr = input("\n\33[1m\33[34m What operation would you like to perform? Enter Add = '+'; Subtraction = '-'; Multiplication = '*'; Division = '/': \33[0m")
             if ask_usr not in ["+", "-", "*", "/"]:
                 raise ValueError
         except ValueError:
              print("\n\33[1m\33[31mInvalid Operation. Try again.\33[0m")
+        return self.operation
+    def usr_input(self):
+        try:
+            number_1 = float(input("\n\33[43m1st number: \33[0m"))
+            number_2 = float(input("\n\33[43m2nd number: \33[0m"))
+            print()
+        except ValueError:
+            print("\n\33[1m\33[31mThis calculator only accepts numbers\33[0m") 
         return self.input
 # Ask if they want to continue
