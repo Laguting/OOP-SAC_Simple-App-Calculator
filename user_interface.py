02f8d05535689054207f12376f3fe1a1ae791a0d
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-21 14:01:17 +0800

    Added the question the user to input the numbers they have in mind.

--- user_interface.py
+++ user_interface.py
@@ -11,3 +11,9 @@ class Interface():
                 except ValueError:
                     print("\n\33[1m\33[31mInvalid Operation. Try again.\33[0m")
                     continue
+                try:
+                    number_1 = float(input("\n\33[43m1st number: \33[0m"))
+                    number_2 = float(input("\n\33[43m2nd number: \33[0m"))
+                    print()
+                except ValueError:
+                    print("\n\33[1m\33[31mThis calculator only accepts numbers\33[0m") 
