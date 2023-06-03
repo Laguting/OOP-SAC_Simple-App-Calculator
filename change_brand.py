30430788d19755588ba2522beacb83b708aa44e7
Laguting <lagutingmariconjaneg@gmail.com>
2023-06-03 14:34:37 +0800

    Fix the issue in the change_brand file because it print '<pyfiglet.Figlet object at 0x000002A15F593070>' in the terminal

--- change_brand.py
+++ change_brand.py
@@ -2,10 +2,12 @@
 from pyfiglet import Figlet
 from termcolor import colored
 class Brand():
+    def __init__(self, brand):
+        self.brand = brand
     def change_bd(self):
         s_a_c = Figlet(font = "digital", justify = "right")
         print()
         print("⚜ " * 89)
         print()
-        print(colored(s_a_c.renderText("                                                             ======= AETH3R ======="), "red"))
-        return s_a_c
+        print(colored(s_a_c.renderText("                                                            ======= AETH3R ======="), "red"))
+        return self.brand
