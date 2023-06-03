c26bbd4b08b0a58952c8182db67a6338641067ff
Laguting <lagutingmariconjaneg@gmail.com>
2023-06-03 14:22:15 +0800

    "Change the brand of the calculator"

---
+++ change_brand.py
@@ -0,0 +1,11 @@
+# Import calculator_intros
+from pyfiglet import Figlet
+from termcolor import colored
+class Brand():
+    def change_bd(self):
+        s_a_c = Figlet(font = "digital", justify = "right")
+        print()
+        print("⚜ " * 89)
+        print()
+        print(colored(s_a_c.renderText("                                                             ======= AETH3R ======="), "red"))
+        return s_a_c
--- test_calculator.py
+++ test_calculator.py
@@ -4,17 +4,19 @@
 
 from calculator_intros import calc_interface
 from user_interface import Interface
+from retry import Retry
+from change_brand import Brand
 
 class Calculator():
 # Call calculator_intros for the brand, welcome, and loading features
+    ci_2 = Brand()
+    print(ci_2.change_bd()) # Display the brand of the calculator
     ci_1 = calc_interface("","","")
-    print(ci_1.get_brand()) # Display the brand of the calculator
     print(ci_1.show_welcome()) # Welcome the user
     print(ci_1.loading_bar()) # Loading Bar
 # Display the question for operations, and numbers as well as the result of the inputted datas
     perf_calc = Interface("", "")
     print(perf_calc.process()) # add, subtract, multiply, divide display
 # Ask the user if the user want to try again
-    from retry import Retry
     re_1 = Retry("")
     print(re_1.re_try())
